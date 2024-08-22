e1b4dbc7ac5215b600f713bd0d4efab019c4701e
Suraj singh <suraj142005@gamil.com>
2024-08-22 20:01:55 +0530

    section 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Games from './components/Games'
+import Footer from './components/Footer'
 function App() {
   return (
     <div >
       <Navbar />
       <Main/>
       <Games/>
+      <Footer/>
     </div>
   )
 }
--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 
 function Games() {
+    const data = [
+        {
+            img: "/public/1.jpg",
+            title: "Minecraft java & bedrock",
+            tags: "sandbox survival, Adventure",
+            subtags: "single-player + multiplayer",
+        },
+        {
+            img: "/2.avif",
+            title: "Minecraft java & bedrock",
+            tags: "sandbox survival, Adventure",
+            subtags: "single-player + multiplayer",
+        },
+        {
+            img: "/3.avif",
+            title: "Minecraft java & bedrock",
+            tags: "sandbox survival, Adventure",
+            subtags: "single-player + multiplayer",
+        },
+        {
+            img: "/4.avif",
+            title: "Minecraft java & bedrock",
+            tags: "sandbox survival, Adventure",
+            subtags: "single-player + multiplayer",
+        },
+    ];
     return (
-        <div className='min-h-screen w-full bg-[url("/bg2.jpg")]  bg-cover bg-no-repeat bg-center'>
-            <div className="min-h-screen w-full bg-[#111111be] text-white jersey-10-regular font-bold text-center pt-5">
-                <div className=" text-5xl border-[#ffffff3c] border-b-2">DISCOVER OUR GAMES</div>
+        <div className="min-h-screen w-full bg-[#171615] text-white jersey-10-regular font-bold text-center pt-5">
+            <div className=" text-5xl ">
+                DISCOVER OUR GAMES
+            </div>
+            <div>
+                {data.map((items) => (
+                    <div className=" mt-2 w-full min-h-[65vh] ">
+                        <hr className="border-[#524e4ec2] border-t-2 w-5/6 mx-auto my-8"/>
+                        <img src={items.img} alt="Java and Bedrock" className=" w-10/12 m-auto my-4 mt-8 " />
+                        <div className="font-extralight text-3xl">{items.title}</div>
+                        <div className="text-gray-400 text-sm font-sans">{items.tags}</div>
+                        <div className="text-gray-400 text-sm font-sans">{items.subtags}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
