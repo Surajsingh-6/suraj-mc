0e1488d40216f6642dbe323022dc73326279cbef
Suraj singh <suraj142005@gamil.com>
2024-08-23 07:27:08 +0530

    fixing some bugs

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -6,19 +6,19 @@ function Sections({ title, image, text, url }) {
         setexpand(!expand);
     };
     return (
-        <div className="min-h-24 p-4  border-2 section text-white">
+        <div className="min-h-24 p-4  border-b-2 section top-right text-white border-[#000000] ">
             <div className="flex items-center justify-between ">
-                <img src={image} alt={title} className="h-16" />
-                <div className="ml-2 text-white" onClick={handleExpand}>
+                <img src={image} alt={title} className="h-16" onClick={handleExpand} />
+                <div className="ml-2 text-white jersey-10-regular text-3xl" onClick={handleExpand}>
                     {title} 
                 </div>
-                <div> &#8744;</div>
+                <div onClick={handleExpand} className="text-2xl"> &#8744;</div>
             </div>
             {expand && (
-                <div> 
+                <div className="my-5 p-4"> 
                     <img src={url} alt="title" />
-                    <p>{text}</p>
-                    <button>LEARN MORE &gt; </button>
+                    <p className="text-[#dadada]">{text}</p>
+                    <button className="h-12 border-2 mt-4 w-40  bg-green-800">LEARN MORE &gt; </button>
                 </div>
             )}
         </div>
